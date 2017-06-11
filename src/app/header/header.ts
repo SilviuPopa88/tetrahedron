a91bc3e9121496c41ecd4acd09ad36f7d1e9df9c
Silviu Popa <silviu@product-foundry.com>
2017-06-11 17:26:58 +0300

    Subscribe to router events

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LocalStorageService } from 'angular-2-local-storage';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'tetra-header',
@@ -8,21 +9,37 @@ import { LocalStorageService } from 'angular-2-local-storage';
 export class HeaderComponent {
   public isScrolledTop: boolean = true;
   public activeButton: string = '';
+  private routerObservable: any;
 
-  constructor(private localStorageService: LocalStorageService) {
-    let results: Array<String> = document.location.href.split('#');
-    let linkParam: string = null;
-    if (results[1]) {
-      linkParam = results[1].split('/')[1];
-    }
-    let localActiveButton: string = localStorageService.get('activeButton') ? localStorageService.get('activeButton').toString() : linkParam ? linkParam : 'home';
-    this.activeButton = document.location.href.indexOf(localActiveButton) !== -1 ? localActiveButton : 'home';
-    if (this.activeButton !== 'home') {
-      this.isScrolledTop = false;
+  constructor(private localStorageService: LocalStorageService, private router: Router) {
+    //let results: Array<String> = document.location.href.split('#');
+    //this.setScrollAfterInit(results[1]);
+  }
+
+  ngOnInit() {
+    this.routerObservable = this.router.events.subscribe((event: any) => {
+
+      this.setScrollAfterInit(event.url);
+    });
+  }
+
+  ngOnDestroy() {
+    if(this.routerObservable != null) {
+      this.routerObservable.unsubscribe();
     }
   }
 
-  scrollHandler(event) {
+  setScrollAfterInit(url) {
+    let linkParam: string = null;
+    if (url) {
+      linkParam = url.split('/')[1];
+    }
+    let localActiveButton: string = linkParam ? linkParam : this.localStorageService.get('activeButton') ? this.localStorageService.get('activeButton').toString() : 'home';
+    this.activeButton = document.location.href.indexOf(localActiveButton) !== -1 ? localActiveButton : 'home';
+    this.isScrolledTop = this.activeButton === 'home' ? true : false;
+  }
+
+  scrollHandler(event: any) {
     let number = event.target.body.scrollTop;
     if (number > 100) {
       this.isScrolledTop = false;
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,21 @@
 import {Component} from '@angular/core';
+import { Router } from '@angular/router';
+import {HeaderComponent} from '../header/header';
 
 @Component({
   selector: 'tetra-home',
   template: require('./home.html')
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  private header: HeaderComponent;
+
+  constructor(private router: Router) {
+
+  }
+
+  gotToAbout() {
+    //this.header.setActiveHeader("about");
+    this.router.navigate(['/about']);
+  }
+}
